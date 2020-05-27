23aa95716909ff6d9bf809c8d43c45f8a4ad88c5
Samuel Ezedi <samuelezedi@gmail.com>
2020-05-27 04:28:50 +0100

    upgrades

---
+++ lib/views/widgets/navigations/navigationbartablet.dart
@@ -0,0 +1,25 @@
+import 'package:firstpwa/views/widgets/navigations/nav_bar_logo.dart';
+import 'package:firstpwa/views/widgets/navigations/navbaritem.dart';
+import 'package:flutter/material.dart';
+
+class NavigationBarTabletDesktop extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+            NavBarLogo(),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                NavBarItem('Episodes'),
+                SizedBox(width: 60,),
+                NavBarItem('About'),
+            ],)
+        ],
+        ),
+    );
+  }
+}
