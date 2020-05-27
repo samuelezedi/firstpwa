719402aa6afd542e201741314c07dbe2a55b2cb1
Samuel Ezedi <samuelezedi@gmail.com>
2020-05-27 04:32:52 +0100

    updates

--- lib/views/widgets/navigations/navigation.dart
+++ lib/views/widgets/navigations/navigation.dart
@@ -1,25 +1,15 @@
 import 'package:firstpwa/views/widgets/navigations/nav_bar_logo.dart';
 import 'package:firstpwa/views/widgets/navigations/navbaritem.dart';
+import 'package:firstpwa/views/widgets/navigations/navigationbartablet.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class NavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-            NavBarLogo(),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                NavBarItem('Episodes'),
-                SizedBox(width: 60,),
-                NavBarItem('About'),
-            ],)
-        ],
-        ),
+    return ScreenTypeLayout(
+      mobile: NavigationBarMobile(),
+      tablet: NavigationBarTabletDesktop(),
     );
   }
 }
---
+++ lib/views/widgets/navigations/navigationbarmobile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavigationBarMobile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+
+      ],),
+    );
+  }
+}
